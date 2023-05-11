55a6b5bdc48458f1c21356e69bb790d66326845a
Roctfore <67772906+Roctfore@users.noreply.github.com>
2023-05-11 08:46:23 -0400

    Add files via upload

--- BDM_FC_yl9908.py
+++ BDM_FC_yl9908.py
@@ -1,101 +1,102 @@
+import csv
+import json
+import numpy as np
+import math
+import pandas as pd
+import datetime
+import pyproj
+import IPython
+
 import pyspark
-import sys
-from datetime import datetime
-from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+sc = pyspark.SparkContext.getOrCreate()
 
-def main(output_folder):
-  # Set up Spark configuration and context
-  conf = SparkConf().setAppName("BDM_FC_yl9908")
-  sc = pyspark.SparkContext.getOrCreate()
+WEEKLY_PATTERN = '/shared/CUSP-GX-6002/data/weekly-patterns-nyc-2019-2020/part-*'
+NYC_SUPERMARKETS = os.getenv('SPARK_YARN_STAGING_DIR')+'/nyc_supermarkets.csv'
+NYC_CBG_CENTROIDS = os.getenv('SPARK_YARN_STAGING_DIR')+'/nyc_cbg_centroids.csv'
 
-  # Load data
-  supermarket = sc.textFile('/shared/CUSP-GX-6002/FC/yl9908/nyc_supermarkets.csv')
-  weekly_patterns = sc.textFile('/shared/CUSP-GX-6002/data/weekly-patterns-nyc-2019-2020/*')
-  nyc_cbg_centroids = sc.textFile('/shared/CUSP-GX-6002/FC/yl9908/nyc_cbg_centroids.csv')
+nyc_supermarkets = sc.textFile(NYC_SUPERMARKETS).filter(lambda x: not x.startswith('place_id,latitude,'))\
+            .map(lambda line: next(csv.reader([line])))
+safegraph_placekeys = set(nyc_supermarkets.map(lambda x: x[-2]).collect())
 
-  # Convert to RDD
-  def parse_csv(line):
-    return tuple(line.strip().split(','))
 
-  supermarket_header = supermarket.first()
-  supermarket = supermarket.filter(lambda x: x != supermarket_header).map(parse_csv)
-  weekly_patterns_header = weekly_patterns.first()
-  weekly_patterns = weekly_patterns.filter(lambda x: x != weekly_patterns_header).map(parse_csv)
-  nyc_cbg_centroids_header = nyc_cbg_centroids.first()
-  nyc_cbg_centroids = nyc_cbg_centroids.filter(lambda x: x != nyc_cbg_centroids_header).map(parse_csv)
+proj = pyproj.Proj(init='EPSG:2263', preserve_units=True)
 
-  # Filter weekly_patterns by placekey
-  supermarket_placekeys = supermarket.map(lambda x: x[0]).collect()
-  weekly_patterns_filtered = weekly_patterns.filter(lambda x: x[0] in supermarket_placekeys)
+nyc_cbg_centroids = sc.textFile(NYC_CBG_CENTROIDS) \
+                     .filter(lambda x: not x.startswith('cbg_fips,')) \
+                     .map(lambda line: next(csv.reader([line])))\
+                     .filter(lambda x: any(x[0].startswith(prefix) for prefix in ['36061','36005','36047','36081','36085']))\
+                     .map(lambda x: (x[0], proj(float(x[2]), float(x[1]))))
+cbgs_geodict = nyc_cbg_centroids.collectAsMap()
+nyc_cbg_fips = set(cbgs_geodict.keys())
 
-  # Filter by date_range_start and date_range_end
-  months_of_interest = ['2019-03-01', '2019-10-01', '2020-03-01', '2020-10-01']
-  weekly_patterns_filtered = weekly_patterns_filtered.filter(lambda x: x[1][:7] in months_of_interest or x[2][:7] in months_of_interest)
 
-  # Extract CBG FIPS codes
-  def extract_cbg_fips(visitor_home_cbgs_str):
-    return visitor_home_cbgs_str.strip().split(':')[0][1:]
+start_dates = [datetime.date(2019, 3, 1), datetime.date(2019, 10, 1), datetime.date(2020, 3, 1), datetime.date(2020, 10, 1)]
+end_dates = [datetime.date(2019, 4, 1), datetime.date(2019, 11, 1), datetime.date(2020, 4, 1), datetime.date(2020, 11, 1)]
+date_label = ['2019-03', '2019-10', '2020-03', '2020-10']
 
-  weekly_patterns_cbg_fips = weekly_patterns_filtered.map(lambda x: (x[0], x[1], x[2], extract_cbg_fips(x[16])))
+def parse_date(datestr):
+    return datetime.datetime.strptime(datestr[:10], '%Y-%m-%d').date()
+def filter_by_date_range(record):
+    start_date = parse_date(record[12])
+    end_date = parse_date(record[13])
+    for i in range(4):
+        if start_dates[i] <= start_date < end_dates[i] or start_dates[i] <= end_date < end_dates[i]:
+            return True
+    return False
+def label_date_range(start_date, end_date):
+    start_date = parse_date(start_date)
+    end_date = parse_date(end_date)
+    # label date with: 2019-03	2019-10	2020-03	2020-10
+    for i in range(4):
+        if start_dates[i] <= start_date < end_dates[i] or start_dates[i] <= end_date < end_dates[i]:
+            return date_label[i]
+    return 'None'
 
-  # Filter nyc_cbg_centroids
-  def nyc_cbg_filter(cbg_fips):
-    cbg_fips = cbg_fips[0]
-    return cbg_fips.startswith('36061') or cbg_fips.startswith('36005') or cbg_fips.startswith('36047') or cbg_fips.startswith('36081') or cbg_fips.startswith('36085')
+def filter_cbgs(home_cbgs_dict, nyc_cbg_fips):
+  # first filter by borough to reduce the number of times to traverse the fips list
+  borough_keys = [k for k in home_cbgs_dict.keys() if k[:5] in ['36061', '36005', '36047', '36081', '36085']]
+  # then filter by fips in nyc_cbg_centroids
+  fips_keys = [k for k in borough_keys if k in nyc_cbg_fips]
 
-  nyc_cbg_centroids_filtered = nyc_cbg_centroids.filter(nyc_cbg_filter)
+  filtered_dict = {k: home_cbgs_dict[k] for k in fips_keys}
 
-  # Join weekly_patterns_cbg_fips with nyc_cbg_centroids_filtered
-  joined_data = weekly_patterns_cbg_fips.map(lambda x: (x[2], x)).join(nyc_cbg_centroids_filtered.map(lambda x: (x[0], x)))
+  return filtered_dict
 
-  # Define haversine distance function
-  def distance(lat1, lon1, lat2, lon2):
-    # Calculate the difference in latitudes and longitudes
-    delta_lat = lat2 - lat1
-    delta_lon = lon2 - lon1
 
-    # Calculate the great circle distance between the two points using the Haversine formula
-    a = math.sin(delta_lat/2)**2 + math.cos(lat1_rad)*math.cos(lat2_rad)*math.sin(delta_lon/2)**2
-    c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
-    distance = 3961*c # Radius of Earth in miles
-    return distance
+def distance(coordinate1,coordinate2):
+  x1, y1 = coordinate1
+  x2, y2 = coordinate2
+  return 0.000621371192*((x1 - x2)**2 + (y1 - y2)**2)**0.5
 
-  # Calculate haversine distance for each row
+def get_distance_for_each_month(values):
+  value_list = ['','','','']
+  month = ['2019-03', '2019-10', '2020-03', '2020-10']
+  for record in values:
+    for i in range(4):
+      if record[0] == month[i]:
+        value_list[i] = record[1]
+  return tuple(value_list)
 
-  def compute_distance(row):
-    _, ((_, date_range_start, date_range_end, visitor_home_cbgs), (_, lat1, lon1)) = row
-    lat2, lon2 = float(lat1), float(lon1)
-    lat1, lon1 = float(row[1][0][3]), float(row[1][0][4])
-    distance = distance(lat1, lon1, lat2, lon2)
-    return visitor_home_cbgs, date_range_start[:7], date_range_end[:7], distance, int(row[1][0][14])
+weekly_pattern = sc.textFile(WEEKLY_PATTERN).filter(lambda x: not x.startswith('"placekey","'))\
+            .map(lambda line: next(csv.reader([line])))\
+            .filter(lambda x: x[0] in safegraph_placekeys)\
+            .filter(filter_by_date_range)\
+            .map(lambda x: (x[18], filter_cbgs(json.loads(x[19]), nyc_cbg_fips), label_date_range(x[12],x[13])))\
+            .filter(lambda x: not x[1]=={})\
+            .map(lambda x: ({(k,x[2]): (distance(cbgs_geodict[x[0]],cbgs_geodict[k]),x[1][k]) for k in x[1].keys()}))\
+            .flatMap(lambda x: [(k, x[k]) for k in x.keys()])\
+            .groupByKey()\
+            .mapValues(lambda values: sum([distance * weight for distance, weight in values]) / sum([weight for distance, weight in values]))\
+            .map(lambda x: (x[0][0], (x[0][1], x[1])))\
+            .groupByKey()\
+            .mapValues(lambda values: get_distance_for_each_month(values))\
+            .map(lambda x: (x[0], x[1][0], x[1][1], x[1][2], x[1][3]))\
+            .sortBy(lambda x: x[0])
 
-  # Compute distance for each row and filter out rows with zero raw_visitor_counts
-  distances = joined_data.map(compute_distance).filter(lambda x: x[4] > 0)
-
-  # Calculate total and average distance
-  def compute_totals(row):
-    visitor_home_cbgs, date_range_start, date_range_end, distance, visitor_count = row
-    return (visitor_home_cbgs, date_range_start, date_range_end), (distance * visitor_count, visitor_count)
-
-  totals = distances.map(compute_totals).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-
-  # Calculate average distance
-  averages = totals.map(lambda x: (x[0], x[1][0] / x[1][1]))
-
-  # Pivot the data
-  def pivot_key(row):
-    visitor_home_cbgs, date_range_start, date_range_end = row[0]
-    return visitor_home_cbgs, (date_range_start, row[1])
-
-  pivoted_data = averages.map(pivot_key).groupByKey().mapValues(dict)
-
-  # Save output
-  def format_output(row):
-    return f"{row[0]},{row[1].get('2019-03', '')},{row[1].get('2019-10', '')},{row[1].get('2020-03', '')},{row[1].get('2020-10', '')}"
-
-  output = pivoted_data.map(format_output)
-  output.saveAsTextFile(output_folder)
-
-if __name__ == '__main__':
-    output_folder = sys.argv[1]
-    main(output_folder)
+header = 'cbg_fips,2019-03,2019-10,2020-03,2020-10'
+weekly_pattern.coalesce(1)\
+    .mapPartitions(lambda x: itertools.chain([header], x)) \
+    .saveAsTextFile(sys.argv[1] if len(sys.argv)>1 else 'outputfc')
